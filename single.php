b223958320e7496507b4e7b6d34be218dbb35c5d
Rob Monhemius <robmonhemius@gmail.com>
2021-09-20 09:36:35 +0100

    created single-page

--- single.php
+++ single.php
@@ -29,7 +29,21 @@
       </ul>
     </div>
     <main class="w3-col m9 l9">
-      HELLO
+      <article class="post">
+        <div class="meta">Posted at 2:00pm on 03-29-2017 by Brad</div>
+        <hr>
+        <img class='w3-animate-right' src="<?php echo get_stylesheet_directory_uri() ;?>/images/1.jpg" alt="">
+        <div class="w3-row">
+          <div class="w3-col l2">
+            <br>
+            <a class='w3-btn w3-red' href="<?php echo home_url(); ?>">Back</a>
+          </div>
+          <div class="w3-col l10">
+            <h1>Photo Title</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis reiciendis maxime nihil. Iste a culpa omnis debitis laudantium. Quaerat sint quam cupiditate vitae explicabo accusamus optio ut vel laboriosam voluptate?</p>
+          </div>
+        </div>
+      </article>
     </main>
   </div>
   <footer>
