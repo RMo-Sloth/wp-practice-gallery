de4d2da41520d756aa9fc81581f0515f195b846e
Rob Monhemius <robmonhemius@gmail.com>
2021-09-20 07:45:14 +0100

    created header

---
+++ index.php
@@ -0,0 +1,12 @@
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>" >
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+  <title><?php the_title(); ?></title>
+</head>
+<body>
+  INDEX
+</body>
+</html>
