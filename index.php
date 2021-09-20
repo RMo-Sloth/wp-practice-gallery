81dc71ffd6899611b299c27f32f86337320d546d
Rob Monhemius <robmonhemius@gmail.com>
2021-09-20 08:36:12 +0100

    adding images

--- index.php
+++ index.php
@@ -31,10 +31,28 @@
     <main class="w3-col m9 l9">
       <div class="w3-row">
         <div class="w3-col m4 l4">
+          <img src="<?php echo get_stylesheet_directory_uri() ;?>/images/1.jpg" alt="">
+          <p>Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus. Sed hendrerit. Ut a nisl id ante tempus hendrerit.</p>
         </div>
         <div class="w3-col m4 l4">
+          <img src="<?php echo get_stylesheet_directory_uri() ;?>/images/2.jpg" alt="">
+          <p>Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus. Sed hendrerit. Ut a nisl id ante tempus hendrerit.</p>
         </div>
         <div class="w3-col m4 l4">
+          <img src="<?php echo get_stylesheet_directory_uri() ;?>/images/3.jpg" alt="">
+          <p>Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus. Sed hendrerit. Ut a nisl id ante tempus hendrerit.</p>
+        </div>
+        <div class="w3-col m4 l4">
+          <img src="<?php echo get_stylesheet_directory_uri() ;?>/images/4.jpg" alt="">
+          <p>Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus. Sed hendrerit. Ut a nisl id ante tempus hendrerit.</p>
+        </div>
+        <div class="w3-col m4 l4">
+          <img src="<?php echo get_stylesheet_directory_uri() ;?>/images/5.jpg" alt="">
+          <p>Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus. Sed hendrerit. Ut a nisl id ante tempus hendrerit.</p>
+        </div>
+        <div class="w3-col m4 l4">
+          <img src="<?php echo get_stylesheet_directory_uri() ;?>/images/6.jpg" alt="">
+          <p>Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus. Sed hendrerit. Ut a nisl id ante tempus hendrerit.</p>
         </div>
       </div>
     </main>
