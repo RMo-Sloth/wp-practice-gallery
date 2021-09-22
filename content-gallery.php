5aba7fca3a53ae811ac415f1aca8d2dc9ef81e17
Rob Monhemius <robmonhemius@gmail.com>
2021-09-22 01:48:44 +0100

    gallery items

--- content-gallery.php
+++ content-gallery.php
@@ -1,1 +1,12 @@
-PHOTO
+<div class="w3-col m4 l4 pic">
+  <?php 
+    if(has_post_thumbnail()):
+    printf(
+      '<div class="post-thumbnail"><a href="%s">%s</a></div>',
+      get_the_permalink(),
+      get_the_post_thumbnail( $id, 'large', ['class' => 'w3-animate-opacity w3-hover-opacity'])
+    );
+  endif; 
+  the_content();
+  ?>
+</div>
--- content.php
+++ content.php
@@ -1,4 +1,14 @@
 <article class="post">
   <?php the_title( '<h2>', '</h2>' ); ?>
   <p class="meta">Posted at <?php the_time(); ?> at <?php the_date(); ?> by <a href="<?php echo get_author_posts_url( get_the_author_meta('ID') ); ?>"><?php the_author(); ?></a></p>
+  <?php 
+  if( has_post_thumbnail() ):
+    printf(
+      '<div class="post-thumbnail">%s</div>', 
+      get_the_post_thumbnail()
+    );
+  endif;
+  print('<br>');
+  the_content();
+  ?>
 </article>
