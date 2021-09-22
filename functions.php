baccdfaa92ce20140b5dbcfd1a3afbf0b936fffa
Rob Monhemius <robmonhemius@gmail.com>
2021-09-22 05:57:38 +0100

    search + widget

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('widgets/class-wp-widget-categories.php');
+
 function theme_setup() {
   add_theme_support('post-thumbnails');
   set_post_thumbnail_size( 900, 600 );
@@ -7,3 +9,12 @@ function theme_setup() {
   add_theme_support('post-formats', ['gallery']);
 }
 add_action( 'after_setup_theme', 'theme_setup' );
+
+function theme_widgets() {
+  register_sidebar( [
+    'name' => __('Sidebar'),
+    'id' => 'sidebar'
+  ] );
+  register_widget('WP_Widget_Categories_Custom');
+}
+add_action('widgets_init', 'theme_widgets');
